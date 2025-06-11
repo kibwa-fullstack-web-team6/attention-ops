46ca064b349dd1223d203defab7766c339e4eac0
AnneMaier <hhch98@gmail.com>
2025-06-11 01:47:42 +0000

    feat 자동 배포 sh 추가

---
+++ sh/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use node
+
+echo $NVM_DIR
+
+cd /work/attention
+
+PROJECT_SRC=/work/attention
+DEST=/$HOME/deploy
+
+rm -rf $DEST
+mkdir -p $DEST
+cp -rf $SRC $DEST
+
+cd $DEST/nodejs
+
+source ~/.bashrc
+
+npm install
+
+pm2 restart all
